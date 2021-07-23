d3a42badc61776f84347583158ddeb8a4b912dcd
up456 <79118046+up456@users.noreply.github.com>
2021-07-24 00:56:28 +0900

    완성

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log(123);
+const toggleBtn = document.querySelector('.fa-bars');
+const menu = document.querySelector('.header__menu-container');
+const icons = document.querySelector('.header__sub-container');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  icons.classList.toggle('active');
+});
